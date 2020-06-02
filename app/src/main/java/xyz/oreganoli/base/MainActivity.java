2533d4802d0833e52fb9c3b8d8ea51b4e673f90d
oreganoli <3611916+oreganoli@users.noreply.github.com>
2020-06-02 05:12:10 +0200

    added view fields

--- app/src/main/java/xyz/oreganoli/base/MainActivity.java
+++ app/src/main/java/xyz/oreganoli/base/MainActivity.java
@@ -1,13 +1,38 @@
 package xyz.oreganoli.base;
 
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    EditText communeInput;
+    EditText countyInput;
+    EditText provinceInput;
+    EditText countryInput;
+    EditText continentInput;
+    Spinner criterion;
+    EditText searchInput;
+    CheckBox oneRow;
+    LinearLayout list;
 
+    private void getWidgets() {
+        communeInput = findViewById(R.id.communeInput);
+        countyInput = findViewById(R.id.countyInput);
+        provinceInput = findViewById(R.id.provinceInput);
+        countryInput = findViewById(R.id.countryInput);
+        continentInput = findViewById(R.id.continentInput);
+        criterion = findViewById(R.id.criterion);
+        searchInput = findViewById(R.id.searchInput);
+        oneRow = findViewById(R.id.oneRow);
+        list = findViewById(R.id.list);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWidgets();
     }
 }
